a3c051968c0046e5998b87c34958559ffc5ec769
Nikita <tonno651@gmail.com>
2023-01-22 20:15:19 +0300

    useRef

--- src/app/components/exercises/useRefExercise.jsx
+++ src/app/components/exercises/useRefExercise.jsx
@@ -1,6 +1,29 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import CollapseWrapper from "../common/collapse";
 const UseRefExercise = () => {
+    const [isClicked, setIsClicked] = useState(false);
+
+    const block = useRef(null);
+
+    const handleClick = () => {
+        let height, width, innerHTML;
+        if (!isClicked) {
+            height = "150px";
+            width = "80px";
+            innerHTML = "<small>Text</small>";
+        } else {
+            height = "40px";
+            width = "60px";
+            innerHTML = "<small>Блок</small>";
+        }
+
+        block.current.style.height = height;
+        block.current.style.width = width;
+        block.current.innerHTML = innerHTML;
+
+        setIsClicked((prevState) => !prevState);
+    };
+
     return (
         <CollapseWrapper title="Упражнение">
             <p className="mt-3">
@@ -12,6 +35,7 @@ const UseRefExercise = () => {
                 <li>высота и ширина станут равны 150 и 80 соответственно</li>
             </ul>
             <div
+                ref={block}
                 className="bg-primary d-flex flex-row justify-content-center align-items-center rounded"
                 style={{
                     height: 40,
@@ -21,6 +45,9 @@ const UseRefExercise = () => {
             >
                 <small>Блок</small>
             </div>
+            <button className="btn btn-primary mt-2" onClick={handleClick}>
+                Изменить блок
+            </button>
         </CollapseWrapper>
     );
 };
